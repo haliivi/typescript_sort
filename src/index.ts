3e8202de37966478293c6c402c8f21f3732fb611
= <haliivi@mail.ru>
2024-05-07 11:07:49 +0700

    Divide logic. Finish project.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-console.log('hey there');
-const foo = 'bar';
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection: NumbersCollection = new NumbersCollection([3, -3])
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(sorter.collection);
